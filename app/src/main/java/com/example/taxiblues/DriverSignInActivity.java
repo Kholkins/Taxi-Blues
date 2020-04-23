0b6a7c316409fa3560cf4a9e6c770ab04890f571
Kholkin <kholkins@mail.ru>
2020-04-23 19:36:26 +0300

    new

--- app/src/main/java/com/example/taxiblues/DriverSignInActivity.java
+++ app/src/main/java/com/example/taxiblues/DriverSignInActivity.java
@@ -2,13 +2,56 @@ package com.example.taxiblues;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DriverSignInActivity extends AppCompatActivity {
+
+    private static final String TAG = "DriverSignInActivity";
+
+    private TextInputLayout textInputEmail;
+    private TextInputLayout textInputName;
+    private TextInputLayout textInputPassword;
+    private TextInputLayout textInputConfirmPassword;
+
+    private Button loginSignUpButton;
+    private TextView toggleLoginSignUpTextView;
+
+    private boolean isLoginModeActive;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_sign_in);
+
+
+//        auth = FirebaseAuth.getInstance();
+//
+//        if (auth.getCurrentUser() != null) {
+//            startActivity(new Intent(DriverSignInActivity.this,
+//                    DriverMapsActivity.class));
+//        }
+
+        textInputEmail = findViewById(R.id.textInputEmail);
+        textInputName = findViewById(R.id.textInputName);
+        textInputPassword = findViewById(R.id.textInputPassword);
+        textInputConfirmPassword = findViewById(R.id.textInputConfirmPassword);
+
+        loginSignUpButton = findViewById(R.id.loginSignUpButton);
+        toggleLoginSignUpTextView = findViewById(R.id.toggleLoginSignUpTextView);
+    }
+
+    public void toggleLoginSignUp(View view) {
+    }
+
+    public void loginSignUpUser(View view) {
     }
 }
