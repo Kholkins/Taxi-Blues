e84c1644b0b8f554be1a411c982c27850fe88131
Kholkin <kholkins@mail.ru>
2020-04-23 18:03:40 +0300

    new

--- app/src/main/java/com/example/taxiblues/ChooseModeActivity.java
+++ app/src/main/java/com/example/taxiblues/ChooseModeActivity.java
@@ -3,6 +3,7 @@ package com.example.taxiblues;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ChooseModeActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class ChooseModeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_mode);
+    }
+
+    public void goToPassengerSignIn(View view) {
     }
 }
