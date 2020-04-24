bc28ff36c3cf7ce6fcceb15952f2139178ef86a1
Kholkin <kholkins@mail.ru>
2020-04-24 12:17:48 +0300

    new

--- app/src/main/java/com/example/taxiblues/DriverSignInActivity.java
+++ app/src/main/java/com/example/taxiblues/DriverSignInActivity.java
@@ -1,15 +1,22 @@
 package com.example.taxiblues;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class DriverSignInActivity extends AppCompatActivity {
 
@@ -82,8 +89,7 @@ public class DriverSignInActivity extends AppCompatActivity {
 
     private boolean validatePassword() {
 
-        String passwordInput = textInputPassword.getEditText().getText()
-                .toString().trim();
+        String passwordInput = textInputPassword.getEditText().getText().toString().trim();
 
         if (passwordInput.isEmpty()) {
             textInputPassword.setError("Please input your password");
@@ -135,6 +141,26 @@ public class DriverSignInActivity extends AppCompatActivity {
             return;
         }
 
+        auth.createUserWithEmailAndPassword(textInputEmail.getEditText().getText().toString().trim(), textInputPassword.getEditText().getText().toString().trim())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = auth.getCurrentUser();
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(DriverSignInActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
 
     }
 }
